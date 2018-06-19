486a790dd2f705c2bf11399fb189b5207655c5c3
YaroslavO <yaroslavo@softwareplanetgroup.com>
2018-06-19 16:31:32 +0300

    add empty line

--- app/endpoints/user/boards.rb
+++ app/endpoints/user/boards.rb
@@ -1,8 +1,8 @@
 module User
   class Boards < Model::Show
     validate_fields :id
+    
     def fetch
-
       user_id = params[:id]
       through = DB[:users_boards].where(user_id: user_id)
       DB[:boards].where(id: through.select(:board_id)).to_a
